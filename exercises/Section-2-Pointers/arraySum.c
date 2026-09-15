cf8b910b69eec5871e508c04c3053915d88720d5
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-15 15:00:40 +0530

    array sum and moving pointers through array

--- exercises/Section-2-Pointers/arraySum.c
+++ exercises/Section-2-Pointers/arraySum.c
@@ -8,9 +8,23 @@ int calculate_array_sum(const int *arr, int size) {
     return sum;
 }
 
+void walk_array(const int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d \n", *arr);
+        arr++;
+    }
+}
+
 int main() {
     int numbers[] = {10, 20, 30, 40, 50};
+    int *p = numbers;
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    int summation = calculate_array_sum(numbers,size);
-    printf("%d", summation);
+    walk_array(p,size);
 }
+
+// int main() {
+//     int numbers[] = {10, 20, 30, 40, 50};
+//     int size = sizeof(numbers) / sizeof(numbers[0]);
+//     int summation = calculate_array_sum(numbers,size);
+//     printf("%d", summation);
+// }
