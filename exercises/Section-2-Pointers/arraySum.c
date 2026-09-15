e29e74a18b9081c954cfca5854d8ca7e906e4b6f
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-15 16:45:22 +0530

    array_modifications_with_pointer

--- exercises/Section-2-Pointers/arraySum.c
+++ exercises/Section-2-Pointers/arraySum.c
@@ -14,13 +14,49 @@ void walk_array(const int *arr, int size) {
         arr++;
     }
 }
+// modifies the array
+void double_array(int *arr, int size) {
+    for (int i = 0; i < size; i++) {
+        (*arr) *= 2;
+        printf("%d \n", *arr);
+        arr++;
+    }
+}
+
+// finding max pointer
+int find_max_pointer(const int*arr, int size) {
+    
+    int max = *arr;
+    for (int i = 1; i < size; i++) {
+    if (*arr > max) {
+        max = *arr;
+    }
+    arr++;
+    }
+    return max;
+}
 
 int main() {
-    int numbers[] = {10, 20, 30, 40, 50};
+    int numbers[] = {12, 5, 27, 8, 19};
     int *p = numbers;
     int size = sizeof(numbers) / sizeof(numbers[0]);
-    walk_array(p,size);
+    int max_pointer = find_max_pointer(p,size);
+    printf("Max pointer is %d",max_pointer);
 }
+
+// int main() {
+//     int numbers[] = {1, 2, 3, 4, 5};
+//     int *p = numbers;
+//     int size = sizeof(numbers) / sizeof(numbers[0]);
+//     double_array(p,size);
+// }
+
+// int main() {
+//     int numbers[] = {10, 20, 30, 40, 50};
+//     int *p = numbers;
+//     int size = sizeof(numbers) / sizeof(numbers[0]);
+//     walk_array(p,size);
+// }
 
 // int main() {
 //     int numbers[] = {10, 20, 30, 40, 50};
