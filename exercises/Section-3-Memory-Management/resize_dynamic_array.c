eae75c51928968badf115df8c8637450e3e362a1
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-16 19:29:48 +0530

    resizing using realloc

---
+++ exercises/Section-3-Memory-Management/resize_dynamic_array.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int *create_array(int size) {
+    int *arr = malloc(size * sizeof(int));
+    return arr;
+}
+
+int *resize_array(int *arr, int old_size, int new_size) {
+    int *temp = realloc(arr, new_size*sizeof(*arr));
+    if (temp == NULL) {
+        // allocation failed
+        return arr;
+    }
+    return temp;
+}
+
+int main() {
+    int *numbers = create_array(3);
+    numbers[0] = 10;
+    numbers[1] = 20;
+    numbers[2] = 30;
+
+    numbers = resize_array(numbers, 3, 5);
+
+    numbers[3] = 40;
+    numbers[4] = 50;
+
+    for (int i = 0; i < 5; i++) {
+    printf("%d ", numbers[i]);
+    }
+
+    free(numbers);
+
+    return 0;
+}
