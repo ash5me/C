b2c64ff15a0ba635e508a097e9bab589de8b83fc
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-18 16:25:48 +0530

    Struct - memory allocation

---
+++ exercises/section-4-Structs/structs.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct {
+    int id;
+    int health;
+    int mana;
+} Player;
+
+Player *create_player(int id, int health, int mana) {
+    Player *ptr = malloc(sizeof(Player));
+    ptr->id = id;
+    ptr->health = health;
+    ptr->mana = mana;
+    return ptr;
+}
+
+int main() {
+    Player *player = create_player(1, 100, 50);
+    printf("ID: %d\n", player->id);
+    printf("Health: %d\n", player->health);
+    printf("Mana: %d\n", player->mana);
+
+    free(player);
+
+    return 0;
+}
