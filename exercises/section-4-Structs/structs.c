f3f6a56d49adab55c8de14a0949a5af2f4aab953
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-18 16:39:38 +0530

    structs - error handling

--- exercises/section-4-Structs/structs.c
+++ exercises/section-4-Structs/structs.c
@@ -9,6 +9,10 @@ typedef struct {
 
 Player *create_player(int id, int health, int mana) {
     Player *ptr = malloc(sizeof(Player));
+    if (ptr == NULL) {
+        printf("Memory allocation failed. \n");
+        return NULL;
+    }
     ptr->id = id;
     ptr->health = health;
     ptr->mana = mana;
@@ -17,6 +21,9 @@ Player *create_player(int id, int health, int mana) {
 
 int main() {
     Player *player = create_player(1, 100, 50);
+    if (player == NULL) {
+        return 1;
+    }
     printf("ID: %d\n", player->id);
     printf("Health: %d\n", player->health);
     printf("Mana: %d\n", player->mana);
