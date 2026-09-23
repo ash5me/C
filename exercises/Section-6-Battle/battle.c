d6ac69329ec92f461e7ebe7fde0db3f1f08366a2
ash5me <ashokkumarmaharajan@gmail.com>
2026-09-23 15:58:17 +0530

    Battle - attack

--- exercises/Section-6-Battle/battle.c
+++ exercises/Section-6-Battle/battle.c
@@ -8,7 +8,20 @@ typedef struct {
     int defense;
 } Character;
 
-void character_attack(Character *attacker, Character *target);
+void character_attack(Character *attacker, Character *target) {
+    // damage = attack - defense (only be positive number)
+    int damage = attacker->attack - target->defense;
+
+    if (damage > 0) {
+        if (target->health > damage) {
+            target->health -= damage; // reducing health by the damage taken for the target
+        } else {
+            target->health = 0;
+        }
+    }
+    
+    return;
+}
 
 void character_heal(Character *character, int amount) {
     //add amount and health //if total exceeds, health = max_health
@@ -26,11 +39,14 @@ int character_is_alive(const Character *character) {
 }
 
 int main() {
-    Character hero = {"Ash",10,100,5,10};
+    Character hero = {"Ash",70,100,6,10};
+    Character enemy = {"Goblin", 50, 50, 3, 7};
 
-    character_heal(&hero,-95);
-
+    character_heal(&hero,95);
     printf("Current health : %d\n", hero.health);
+
+    character_attack(&hero,&enemy);
+    printf("Current defender's health : %d\n", enemy.health);
 
     if(character_is_alive(&hero)) {
         printf("Character is alive %s\n", hero.name);
